8a2cc4e4123c79ed70541d086c12d1cd52255b67
yahya-10 <yahyaakermi1992@gmail.com>
2022-03-15 12:07:18 +0100

    last commit

--- client/__tests__/Login.test.js
+++ client/__tests__/Login.test.js
@@ -9,14 +9,9 @@ import Login from "./src/component/authentication/Login";
 
 afterEach(cleanup);
 
-it("should log the user in after entering the right credentials", async () => {
-  const { getByTestId, getByText } = render(<Login />);
-
-  fireEvent.click(getByTestId("login-user"));
-
-  const pathname = new URL(window.location.pathname);
-
-  const message = await waitForElement(() => getByText(pathname));
-  expect(message).toHaveTextContent(pathname);
+describe("login", () => {
+  test("Login form should be in the document", () => {
+    const loginRenderer = render(<Login />);
+    console.log(loginRenderer);
+  });
 });
-// });
